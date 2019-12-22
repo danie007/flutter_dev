ff5bc028179ee33a4a4bd6feb8ae4858b9ee5a3b
danilselvan@gmail.com <danilselvan@gmail.com>
2019-12-22 15:41:09 +0530

    simple Hello world application

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Center(child: Text("Hello World"))));
+// The main function is the entrance point for all our Flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text("Hello World"),
+      ),
+    ),
+  );
+}
